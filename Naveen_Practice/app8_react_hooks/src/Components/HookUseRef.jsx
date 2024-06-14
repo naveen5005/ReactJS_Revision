b39e2e8820913ecdeab98c59b989b113847a7909
naveen5005 <100987924+naveen5005@users.noreply.github.com>
2024-06-14 08:23:15 +0530

    useRef hook in react

--- Naveen_Practice/app8_react_hooks/src/Components/HookUseRef.jsx
+++ Naveen_Practice/app8_react_hooks/src/Components/HookUseRef.jsx
@@ -1,23 +1,23 @@
 import React, { useEffect, useRef } from 'react'
 
 const HookUseRef = () => {
-    const unameRef = useRef(null);
-    const pwdRef = useRef(null)
-    
+  const unameRef = useRef(null);
+  const pwdRef = useRef(null)
 
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        console.log(unameRef,pwdRef)
-    }
-    useEffect(()=>{
-        unameRef.current.focus(); // to focus the 1st input field
-    },[]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(unameRef, pwdRef)
+  }
+  useEffect(() => {
+    unameRef.current.focus(); // to focus the 1st input field
+  }, []);
 
 
   return (
     <div>
       <form action="" onSubmit={handleSubmit}>
-        Username : <input type="text" ref={unameRef}/> <br />
+        Username : <input type="text" ref={unameRef} /> <br />
         Password :<input type="password" ref={pwdRef} /> <br />
         <input type="submit" value="Submit" />
       </form>
